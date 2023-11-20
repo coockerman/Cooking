5a30760a4ec4e6d0dabc18235778d893dafbb73d
Nguyễn Hải <haibetmen997@gmail.com>
2023-11-20 11:08:43 +0700

    convert to record

--- Cooking/Assets/_Cooker/Scripts/Data/MasterProtocol.cs
+++ Cooking/Assets/_Cooker/Scripts/Data/MasterProtocol.cs
@@ -6,110 +6,32 @@ using UnityEngine;
 
 public class GameScriptableObject : ScriptableObject { }
 
+public abstract record GameData;
 
-public abstract class GameData { }
+public record InitializeGameData(int NCustomer, int NChef, int NChefSupporter, int NDishWasher, int NWaiters, bool IsSuccess) : GameData;
 
-public class InitializeGameData : GameData
-{
-    public int NCustomer { get; }
-    public int NChef { get; }
-    public int NChefSupporter { get; }
-    public int NDishWasher { get; }
-    public int NWaiters { get; }
-    public bool IsSuccess { get; }
-    public InitializeGameData(int nCustomer, int nChef, int nChefSupporter, int nDishWasher, int nWaiters, bool isSuccess)
-    {
-        NCustomer = nCustomer;
-        NChef = nChef;
-        NChefSupporter = nChefSupporter;
-        NDishWasher = nDishWasher;
-        NCustomer = nWaiters;
-        IsSuccess = isSuccess;
-    }
-}
+public abstract record IngredientStatus : GameData;
 
+public record RawIngredient : IngredientStatus;
 
-public abstract class IngredientStatus : GameData { }
+public record ReadyIngredient : IngredientStatus;
 
-public class RawIngredient : IngredientStatus { }
+public abstract record GameAction;
 
-public class ReadyIngredient : IngredientStatus { }
+public record InitializeGame(InitializeGameData InitData, GameAreaManager GameManager) : GameAction;
 
+public record SelectSomeFoods(List<Menu> MenuToSelected) : GameAction;
 
-/// <summary>
-///  GAME ACTIONS
-///  Using for communicate between monoBehaviors and actors
-/// </summary>
-public abstract class GameAction { }
+public record OrderFood(List<Menu> OrderList) : GameAction;
 
-public class InitializeGame : GameAction
-{
-    public InitializeGameData InitData { get; }
-    public GameAreaManager GameManager { get; }
+public record PrepareIngredients(List<Ingredient> RequireIngredients) : GameAction;
 
-    public InitializeGame(InitializeGameData initData, GameAreaManager gameManager) : base()
-    {
-        InitData = initData;
-        GameManager = gameManager;
-    }
-}
+public record OpenRestaurant(List<Menu> MenuByDay, Customer Customer) : GameAction;
 
-public class SelectSomeFoods : GameAction
-{
-    public List<Menu> MenuToSelected;
+public record StartCooking : GameAction;
 
-    public SelectSomeFoods(List<Menu> menuToSelected)
-    {
-        MenuToSelected = menuToSelected;
-    }
-}
+public record SwitchToAutoMode : GameAction;
 
-public class OrderFood : GameAction
-{
-    public List<Menu> OrderList;
-
-    public OrderFood(List<Menu> orderList)
-    {
-        OrderList = orderList;
-    }
-}
-
-public class PrepareIngredients : GameAction
-{
-    public List<Ingredient> RequireIngredients { get; }
-
-    public PrepareIngredients(List<Ingredient> requireIngredients)
-    {
-        RequireIngredients = requireIngredients;
-    }
-}
-
-public class OpenRestaurant : GameAction
-{
-    public List<Menu> MenuByDay { get; }
-    public Customer Customer { get; }
-
-    public OpenRestaurant(List<Menu> menuByDay, Customer customer)
-    {
-        MenuByDay = menuByDay;
-        Customer = customer;
-    }
-}
-public class StartCooking : GameAction
-{
-   
-}
-
-
-
-public class SwitchToAutoMode : GameAction
-{
-    public SwitchToAutoMode() : base() { }
-}
-
-public class SwitchToManualMode : GameAction
-{
-    public SwitchToManualMode() : base() { }
-}
+public record SwitchToManualMode : GameAction;
 
 
